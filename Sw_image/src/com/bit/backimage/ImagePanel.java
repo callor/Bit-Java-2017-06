17fc2e0492c9c8261d1c344d1a11e475e3b42de2
callor <callor@callor.com>
2017-06-09 12:12:05 +0900

    2017-06-09-02

--- Sw_image/src/com/bit/backimage/ImagePanel.java
+++ Sw_image/src/com/bit/backimage/ImagePanel.java
@@ -11,24 +11,14 @@ import javax.swing.JPanel;
 
 /**
  * @author callor JFrame에는 배경이미지를 넣을 수 없어 JPanel을 상속받아 ImagePanel을 만든다.
- * 
- *         배경이미지와 이미지 크기를 생성자 인자로 받아 이미지를 읽고, 설치하고 JFrame에 add 하여 처리한다.
+ * 배경이미지와 이미지 크기를 생성자 인자로 받아 이미지를 읽고, 
+ * 설치하고 JFrame에 add 하여 처리한다.
  *
  */
 public class ImagePanel extends JPanel {
 
 	private Image backgroundImage;
 	private int x, y, width, height;
-
-	@SuppressWarnings("null")
-	public ImagePanel(String fileName, int x, int y, int width, int height) {
-		try {
-			backgroundImage = ImageIO.read(new File(fileName));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	}
 
 	public ImagePanel(String fileName) {
 		try {
